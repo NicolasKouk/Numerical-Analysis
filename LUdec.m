2efe3976ffd639d1780170c9a127af99b2ec8fc3
NicolasKouk <128096300+NicolasKouk@users.noreply.github.com>
2023-10-26 17:29:20 +0300

    Update LUdec.m

--- LUdec.m
+++ LUdec.m
@@ -1,7 +1,8 @@
+% performs an LU decompotition with the Gaussian Elimination method
 function [L , U ] = LUdec ( A )
 n = size (A ,1) ;
 for k =1: n -1
-A ( k +1: n , k ) = A ( k +1: n , k ) / A (k , k ) ;
-A ( k +1: n , k +1: n ) = A ( k +1: n , k +1: n ) - A ( k +1: n , k) * A (k , k +1: n) ;
+  A ( k +1: n , k ) = A ( k +1: n , k ) / A (k , k ) ;
+  A ( k +1: n , k +1: n ) = A ( k +1: n , k +1: n ) - A ( k +1: n , k) * A (k , k +1: n) ;
 end
 L = eye (n , n ) + tril (A , -1) ; U = triu ( A ) ;
